2308f1f2fa367a6ca2c37b0e171e8a9d1ae33d22
Abdelrahman Fahd <aahmed0294@stu.kau.edu.sa>
2025-10-18 23:35:11 +0300

    Time: 0 ms (100%), Space: 10.4 MB (78.06%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,68 @@
+#include <string.h>
+
+int romanToInt(char* s) {
+
+    int length = strlen(s);
+    int output = 0;
+    bool add = true;
+
+    typedef struct {
+
+        char letter;
+        int number;
+
+    } label;
+
+    label arr[7] =
+    { {'I', 1},
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000}};
+
+    for(int i = 0; i < length; i++)
+    {
+        int current = 0;
+        for(int k = 0; k < 7; k++)
+        {
+            if(s[i] == arr[k].letter)
+            {
+                current += arr[k].number;
+                break;
+            }
+
+        }
+
+        int next = 0;
+        for(int j = 0; j < 7; j++)
+        {
+            if(s[i+1] == arr[j].letter)
+            {
+                next += arr[j].number;
+                break;
+            }
+
+        }
+
+
+        if(current > next)
+        {
+            output += current;
+        }
+        else if(current < next)
+        {
+            output += next - current;
+            i++;
+        }
+        else
+        {
+            output += current;
+        }
+    }
+
+
+
+    return output;
+}
